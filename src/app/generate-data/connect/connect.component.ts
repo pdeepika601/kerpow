8ba964659b2bb42e5a23fd28328c06451ef9b223
vaibhavvats85 <vaibhavvats85@gmail.com>
2020-07-10 12:33:00 +0530

    Stepper event added

--- src/app/generate-data/connect/connect.component.ts
+++ src/app/generate-data/connect/connect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-connect',
@@ -6,9 +6,14 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./connect.component.scss']
 })
 export class ConnectComponent implements OnInit {
+
+  @Output() goForward = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
 
+  next(){
+    this.goForward.emit();
+  }
 }
--- src/app/generate-data/generate-data.component.ts
+++ src/app/generate-data/generate-data.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-generate-data',
@@ -10,6 +11,7 @@ export class GenerateDataComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  @ViewChild('stepper', { static: false }) private myStepper: MatStepper;
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -20,6 +22,14 @@ export class GenerateDataComponent implements OnInit {
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+  }
+
+  next() {
+    this.myStepper.next();
+  }
+
+  previous(){
+    this.myStepper.previous();
   }
 
 }
